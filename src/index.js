0c607d5537ad74e29432c66ca504b702489df5c3
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-22 19:12:37 -0300

    feat: create GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from "./components/App"
+import GlobalStyle from './styles/GlobalStyles';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 );
